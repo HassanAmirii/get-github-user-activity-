8f4a481ba24cc977689a18b3fc409dbd006b62f7
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-11-17 13:30:04 +0100

    Fix error handling and response parsing in githubUserActivity function

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ async function githubUserActivity(username) {
       "User-Agent": "githubUserActivity",
       Aceept: "application/vnd.github+json",
     });
+
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error(` error, username: ${username} not found `);
+      }
+      throw new Error(`Api request failed with ${response.status}`);
+    }
+
+    const eventData = await response.json();
+    console.log(eventData);
+    return eventData;
   } catch (error) {
     console.error(error);
   }
 }
+githubUserActivity("HassanAmirii");
